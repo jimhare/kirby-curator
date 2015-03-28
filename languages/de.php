8690f28f24b5d524af9b954d26b7e121b2174e6d
dgdsp <dg@storypioneers.com>
2015-03-28 20:57:27 +0100

    german translations

--- languages/de.php
+++ languages/de.php
@@ -1,22 +1,22 @@
 <?php
 
-l::set('curator.action.addapage', 'Add a Page');
-l::set('curator.action.hidefilters', 'Hide Filters');
+l::set('curator.action.addapage', 'Seite hinzufügen');
+l::set('curator.action.hidefilters', 'Filter ausblenden');
 
-l::set('curator.filter.type', 'Types of Pages');
-l::set('curator.filter.type.all', 'Everything');
-l::set('curator.filter.type.text', 'Texts Only');
-l::set('curator.filter.type.picture', 'Pictures Only');
-l::set('curator.filter.root', 'Root Page');
-l::set('curator.filter.keyword', 'Filter by Keyword');
-l::set('curator.filter.keyword.placeholder', 'Search...');
-l::set('curator.filter.tags', 'Filter by Tags');
-l::set('curator.filter.date.from', 'From Date');
-l::set('curator.filter.date.to', 'To Date');
+l::set('curator.filter.type', 'Seitentypen');
+l::set('curator.filter.type.all', 'Alle');
+l::set('curator.filter.type.text', 'Nur Texte');
+l::set('curator.filter.type.picture', 'Nur Bilder');
+l::set('curator.filter.root', 'Elternseite');
+l::set('curator.filter.keyword', 'Filter nach Schlüsselwort(en)');
+l::set('curator.filter.keyword.placeholder', 'Schlüsselwort(e)');
+l::set('curator.filter.tags', 'Filter nach Schlagwort(en)');
+l::set('curator.filter.date.from', 'Von Datum');
+l::set('curator.filter.date.to', 'Bis Datum');
 
-l::set('curator.results.title.aggregation', 'Preview');  // <label> Preview => Aggregation Preview
-l::set('curator.results.title.curation',    'Filtered Pages');
-l::set('curator.results.empty',             'No matching pages found.');
+l::set('curator.results.title.aggregation', 'Vorschau');  // <label> Preview => Aggregation Preview
+l::set('curator.results.title.curation',    'Gefilterte Seiten');
+l::set('curator.results.empty',             'Pardon, es wurden keine passenden Seiten gefunden.');
 
-l::set('curator.selected.empty', 'No pages selected.');
-l::set('curator.selected.nofigure', 'No Figure');
+l::set('curator.selected.empty', 'Keine Seiten ausgewählt.');
+l::set('curator.selected.nofigure', 'Kein Banner');
